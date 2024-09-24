2e98380fac565e1fe12a50a5d1e1dca132c2b8e6
Alexander <lisitsyn1994digo@gmail.com>
2024-09-25 02:33:24 +0500

    create a server instance and initialize it using the InitRoutes function from the handler package

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
-package cmd
+package main
+
+import (
+	"log"
+	todo "todo-app"
+	"todo-app/pkg/handler"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occurred while running server: %s", err.Error())
+	}
+}
