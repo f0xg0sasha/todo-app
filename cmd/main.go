e2fde3837b5b7f2e49f17c0bcedf46ab9326e01a
Alexander <lisitsyn1994digo@gmail.com>
2024-09-25 23:17:17 +0500

    created dependencies

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 	todo "todo-app"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running server: %s", err.Error())
